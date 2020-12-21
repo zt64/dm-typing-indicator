23395b725e4d5dbd2bdb2cefe9191e5f4a7fbf8d
Litleck <nwowens32@gmail.com>
2020-12-21 16:43:38 -0500

    Clicking indicator goes to typing user

--- components/Typing.jsx
+++ components/Typing.jsx
@@ -1,17 +1,20 @@
-const { React } = require('powercord/webpack');
+const { React, getModule } = require('powercord/webpack');
 const { Tooltip, Spinner } = require('powercord/components');
 
 module.exports = class Component extends React.Component {
   render() {
     const userNames = this.props.users.map(user => user.username);
-    const tooltip = this.props.users.length === 1 ? `${userNames.join(' ')} is typing...` : `${userNames.join(' ')} are typing...`
+    const tooltip = this.props.users.length === 1 ? `${userNames[0]} is typing...` : `${userNames.join(', ')} are typing...`
     const showIndicator = this.props.getSetting('showIndicator', true);
+    const m = getModule([ 'openPrivateChannel' ], false);
 
     return (
-      <Tooltip color='black' position='right' text={tooltip} className='typing-indicator'>
-        {showIndicator && <Spinner type='pulsingEllipsis' animated={true} style={{ marginLeft: 5, opacity: 0.7 }}/>}
-        <h1>{`${this.props.users.length} typing...`}</h1>
-      </Tooltip>
+      <div onClick={async () => this.props.users.length === 1 && await m.openPrivateChannel(this.props.users[0].id)}>
+        <Tooltip color='black' position='right' text={tooltip} className='typing-indicator'>
+          {showIndicator && <Spinner type='pulsingEllipsis' animated={true} style={{ marginLeft: 5, opacity: 0.7 }}/>}
+          <h1>{`${this.props.users.length} typing...`}</h1>
+        </Tooltip>
+      </div> 
     )
   }
 }
