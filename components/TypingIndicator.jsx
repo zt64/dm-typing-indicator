50616265c147d8310237b4fe17dcab79cd182639
litleck <nwowens32@gmail.com>
2021-01-16 13:02:58 -0500

    Added background color customization

--- components/TypingIndicator.jsx
+++ components/TypingIndicator.jsx
@@ -5,9 +5,10 @@ const { Tooltip, Spinner } = require('powercord/components');
 const dmTypingStore = require('../stores/dmTypingStore');
 
 class TypingIndicator extends React.PureComponent {
-  constructor () {
-    super();
+  constructor (props) {
+    super(props);
 
+    this.getSetting = this.props.getSetting;
     this.channelUtils = getModule([ 'openPrivateChannel' ], false);
   }
 
@@ -17,8 +18,8 @@ class TypingIndicator extends React.PureComponent {
 
   renderIndicator (typingUsers) {
     const indicator = [];
-    const indicatorStyle = this.props.getSetting('indicatorStyle', 'icon');
-    const animateIndicator = this.props.getSetting('animateIndicator', true);
+    const indicatorStyle = this.getSetting('indicatorStyle', 'icon');
+    const animateIndicator = this.getSetting('animateIndicator', true);
 
     if (indicatorStyle === 'icon' || indicatorStyle === 'both') {
       indicator.push(<Spinner type='pulsingEllipsis' animated={animateIndicator} style={{ opacity: 0.7, marginBottom: indicatorStyle === 'both' ? 5 : '' }} />);
@@ -56,7 +57,8 @@ class TypingIndicator extends React.PureComponent {
       const tooltipText = typingUsers.length === 1 ? Messages.ONE_USER_TYPING.format({ a: usernames[0] }) : [ strings, 'are typing...' ];
 
       if (this.props.badge) {
-        return <Spinner type='pulsingEllipsis' animated={true} className='dm-typing-badge' itemClassName='dm-typing-badge-spinner' />;
+        const badgeStyle = { backgroundColor: this.getSetting('indicatorBgColor', '#43b581')}
+        return <Spinner type='pulsingEllipsis' animated={true} className='dm-typing-badge' itemClassName='dm-typing-badge-spinner' style={badgeStyle}/>;
       }
 
       return <div className={this.props.className} onClick={this.handleOnClick.bind(this, typingUsers)}>
